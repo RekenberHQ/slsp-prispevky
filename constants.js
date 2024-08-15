0af66ad007e86d38df9cc85e4560ebeaabc3f9b2
Peter Zvirinsky <peter.zvirinsky@gmail.com>
2024-08-15 18:07:45 +0200

    changes

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 export const ZDRAVONA_SITUACIA_TEHOTNA = "Ste momentálne tehotná";
 
-export const STAV_OPATERY_VO_VASEJ_OPATERE = "Vo vašej opatere";
+export const STAV_OPATERY_VO_VASEJ_OPATERE = "V mojej opatere";
 export const STAV_OPATERY_V_OPATERE_OBOCH_RODICOV = "V opatere oboch rodičov";
 
 export const DIETA_ZDRAVONA_SITUACIA_CHRONICKE_OCHORENIE = "Má Vaše dieťa diagnostikované chronické ochorenie alebo postihnutie?";
